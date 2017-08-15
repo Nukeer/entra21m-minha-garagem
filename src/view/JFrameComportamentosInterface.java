d08bb92b5ecfb0c0e939295e61aa392eb0148617
Felipe Vieira Baehr <felipebaehr@gmail.com>
2017-08-15 10:27:11 -0300

    Inicialização do repositório com os seguintes itens:    - Listagem de carros    - Cadastro de carro    - Conexão com o BD

---
+++ src/view/JFrameComportamentosInterface.java
@@ -0,0 +1,20 @@
+package view;
+
+/**
+ *
+ * @author Felipe
+ */
+public interface JFrameComportamentosInterface {
+
+    public void criarTela();
+    
+    public void criarComponentes();
+    
+    public void definirLocalizacao();
+
+    public void adicionarComponentes();
+
+    public void adicionarOnClick();
+
+
+}
